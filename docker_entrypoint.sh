c481c2d1f8a716076c9fa7211fd573a661a2586a
tateisu <tateisu@juggler.jp>
2017-10-09 00:55:46 +0900

    quick fix for https://github.com/tootsuite/mastodon/issues/5274

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -7,8 +7,14 @@
 echo "Creating mastodon user (UID : ${UID} and GID : ${GID})..."
 addgroup -g ${GID} mastodon && adduser -h /mastodon -s /bin/sh -D -G mastodon -u ${UID} mastodon
 
+# set permission
+echo "chgrp ${GID} /mastodon/public/*.html"
+chgrp ${GID} /mastodon/public 
+chgrp ${GID} /mastodon/public/*.html
+
 # echo "Updating permissions..."
 # find /mastodon -path /mastodon/public/system -prune -o -not -user mastodon -not -group mastodon -print0 | xargs -0 chown -f mastodon:mastodon
+
 
 echo "Sleep 5 seconds..."
 sleep 5
